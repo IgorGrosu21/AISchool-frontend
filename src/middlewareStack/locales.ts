11528778c55ea9cfec20280d97b54bcc316ab18b
Igor Grosu <tiktokaye2011@gmail.com>
2025-10-18 15:37:07 +0300

    added sitemap and robots

--- src/middlewareStack/locales.ts
+++ src/middlewareStack/locales.ts
@@ -5,10 +5,12 @@ import { MiddlewareFactory } from './middlewareFactoryType';
  
 const localesMiddleware = createMiddleware(routing);
 
+const excludedRoutes = ['trpc', '_next', '_vercel', 'images', 'favicon.ico', 'logo.png', 'manifest', 'robots.txt', 'sitemap.xml']
+
 export const localesMiddlewareFactory: MiddlewareFactory = (next) => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
     const pathname = request.nextUrl.pathname;
-    if (new RegExp('^(?!(\/(?:api|trpc|_next|_vercel|images|favicon.ico|logo.png|manifest))\/?).*').test(pathname)) {
+    if (new RegExp(`^(?!(\/(?:${excludedRoutes.join('|')}))\/?).*`).test(pathname)) {
       return localesMiddleware(request)
     }
     return next(request, _next);
